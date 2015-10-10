0238c7d391452b68c775db6ddc23bbc23870d90f
Nathan Guerrero <poniesfim@hushmail.com>
2015-10-10 13:38:53 -0700

    Add main batch file and add administrator privileges prompt
    There is now a main batch file, and there is a prompt asking people if
    they have administrator.

---
+++ main.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Have you right clicked the file and clicked "Run As Administrator?" Have you run this script as administrator another way?
+echo If not, exit this script. Otherwise, press enter.
