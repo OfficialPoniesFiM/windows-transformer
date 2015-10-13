1ec48f68a74d8c135b5d0180fc2df1b4814dfbf5
Nathan Guerrero <poniesfim@hushmail.com>
2015-10-12 23:39:09 -0700

    Let user know Chocolatey will be installed

--- main.bat
+++ main.bat
@@ -1,5 +1,10 @@
 @echo off
 cls
+echo This works best with a fresh install.
 echo Have you right clicked the file and clicked "Run As Administrator?" Have you run this script as administrator another way?
+echo If not, exit this script. Otherwise, press enter.
+pause
+echo Chocolatey, a package(program) manager, will be installed.
+echo Is this okay with you?
 echo If not, exit this script. Otherwise, press enter.
 pause
