683def761b0241e997c15c03a8b4ae4f9a467ba3
Nathan Guerrero <poniesfim@hushmail.com>
2015-10-13 06:52:03 -0700

    Chocolatey + Comments

--- main.bat
+++ main.bat
@@ -1,5 +1,7 @@
+:: These are to clear off traces of commands.
 @echo off
 cls
+:: The script will ask the user 2 times if they want to continue running, as well as tell the user about important information.
 echo This works best with a fresh install.
 echo Have you right clicked the file and clicked "Run As Administrator?" Have you run this script as administrator another way?
 echo If not, exit this script. Otherwise, press enter.
@@ -8,3 +10,5 @@ echo Chocolatey, a package(program) manager, will be installed.
 echo Is this okay with you?
 echo If not, exit this script. Otherwise, press enter.
 pause
+:: Chocolatey will be installed to work as a package manager
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
