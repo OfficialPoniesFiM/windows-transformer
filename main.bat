70c2ffaccf052201e327e085d9c09f9d8f737a3c
Nathan Guerrero <poniesfim@hushmail.com>
2015-10-12 23:26:52 -0700

    clear

--- main.bat
+++ main.bat
@@ -1,4 +1,5 @@
 @echo off
+cls
 echo Have you right clicked the file and clicked "Run As Administrator?" Have you run this script as administrator another way?
 echo If not, exit this script. Otherwise, press enter.
 pause
