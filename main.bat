607018d77061f44fa617669b2c52f0a3ec213b1e
Nathan Guerrero <poniesfim@hushmail.com>
2015-10-12 23:23:01 -0700

    Add pause to ensure consent

--- main.bat
+++ main.bat
@@ -1,3 +1,4 @@
 @echo off
 echo Have you right clicked the file and clicked "Run As Administrator?" Have you run this script as administrator another way?
 echo If not, exit this script. Otherwise, press enter.
+pause
