23ed2574e43ba82ea0784b1eeb4b4c3bd809e24f
Nathan Guerrero <poniesfim@hushmail.com>
2015-10-19 15:44:19 -0700

    Formatting + Comment for keyboard shortcuts

--- main.bat
+++ main.bat
@@ -1,6 +1,7 @@
 :: These are to clear off traces of commands.
 @echo off
 cls
+
 :: The script will ask the user 2 times if they want to continue running, as well as tell the user about important information.
 echo This works best with a fresh install.
 echo Have you right clicked the file and clicked "Run As Administrator?" Have you run this script as administrator another way?
@@ -10,8 +11,10 @@ echo Chocolatey, a package(program) manager, will be installed.
 echo Is this okay with you?
 echo If not, exit this script. Otherwise, press enter.
 pause
+
 :: Chocolatey will be installed to work as a package manager
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
 :: We will now install necessary packages.
 choco install chocolateygui -y
 choco install sudo -y
@@ -27,3 +30,5 @@ choco install libreoffice -y
 choco install python -y
 choco install putty -y
 choco install cyg-get -y
+
+:: We will now make new keyboard shortcuts.
